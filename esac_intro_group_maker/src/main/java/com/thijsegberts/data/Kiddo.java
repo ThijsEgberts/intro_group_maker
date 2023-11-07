91b46b75cf9bfcd6858255bafb3efe87728b0f7f
ThijsEgberts <t.t.a.egberts@student.tue.nl>
2023-11-07 18:28:48 +0100

    added first version of kiddo class

---
+++ esac_intro_group_maker/src/main/java/com/thijsegberts/data/Kiddo.java
@@ -0,0 +1,21 @@
+package com.thijsegberts.data;
+
+/**
+ * A data class representing an intro kiddo
+ */
+public class Kiddo {
+    
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private boolean hasToprope;
+    private String diataryPreferences;
+    private boolean[] weekendAvailability;
+    private boolean[] topropeEveningAvailability;
+    
+    public Kiddo() {
+        
+    }
+    
+}
